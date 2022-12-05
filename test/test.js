3889695b70a9428cc5d27ce367622288c707b298
UmamiAppearance <mail@umamiappearance.eu>
2022-12-05 10:24:45 +0100

    arbitrary bytes test

--- test/test.js
+++ test/test.js
@@ -5,14 +5,23 @@ import { promisify } from "util";
 
 const execPromise = promisify(exec);
 
-test("Encoding Base64", async t => {
+test("Encoding Base64 from file", async t => {
     const encoded = (await execPromise("./index.js base64 ./test/fixtures/plain.txt")).stdout;
     const expected = await readFile("./test/fixtures/b64_enc.txt", "utf8");
     t.is(encoded, expected);
 });
 
-test("Decoding Base64", async t => {
-    const decoded = (await execPromise("./index.js base64 ./test/fixtures/b64_enc.txt -d")).stdout;
+test("Decoding Base64 from file", async t => {
+    const cmd = "./index.js base64 ./test/fixtures/b64_enc.txt -d";
+    const decoded = (await execPromise(cmd)).stdout;
     const expected = await readFile("./test/fixtures/plain.txt", "utf8");
     t.is(decoded, expected);
+});
+
+test("En- and decoding arbitrary bytes (Ecoji 2.0)", async t => {
+    const cmd = "./index.js ecoji_v2 ./test/fixtures/bytes.dat | ./index.js ecoji_v2 -d";
+    const bytes = new Uint8Array(await readFile("./test/fixtures/plain.txt"));
+    const backDecoded = new TextEncoder().encode(
+        (await execPromise(cmd)).stdout);
+    t.is(bytes.toString, backDecoded.toString);
 });
