68c60437fb96fc42b2021bb5fb288cbeb87af218
UmamiAppearance <mail@umamiappearance.eu>
2022-12-12 09:41:33 +0100

    v0.2.3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * [BaseExCLI]{@link https://github.com/UmamiAppearance/BaseExCLI}
  *
- * @version 0.2.2
+ * @version 0.2.3
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  */
