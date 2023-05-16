0954342085f46ad07a8cd6d60ae158f22bca5def
UmamiAppearance <mail@umamiappearance.eu>
2023-05-16 14:10:05 +0200

    dependency updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * [BaseExCLI]{@link https://github.com/UmamiAppearance/BaseExCLI}
  *
- * @version 0.3.4
+ * @version 0.3.5
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
